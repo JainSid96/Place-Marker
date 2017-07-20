643a8826537e8ccc465c9c3c49cca3579385fe7d
S:Ddhant Jain <jainsid123456789@gmail.com>
2017-07-20 23:48:03 +0530

    update api repo

--- app/src/main/java/com/example/msjapplication/placemarker/MapsActivity.java
+++ app/src/main/java/com/example/msjapplication/placemarker/MapsActivity.java
@@ -3,6 +3,7 @@ package com.example.msjapplication.placemarker;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -26,6 +27,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -147,6 +149,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         StartActivity.places.add(adress);
         StartActivity.locations.add(latLng);
         StartActivity.arrayAdapter.notifyDataSetChanged();
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.msjapplication.placemarker" , Context.MODE_PRIVATE);
+        try {
+            ArrayList<String> latitudes = new ArrayList<>();
+            ArrayList<String> longitudes = new ArrayList<>();
+            for (LatLng coordinates : StartActivity.locations){
+
+                latitudes.add(Double.toString(coordinates.latitude));
+                longitudes.add(Double.toString(coordinates.longitude));
+            }
+            sharedPreferences.edit().putString("places" , ObjectSerializer.serialize(StartActivity.places)).apply();
+            sharedPreferences.edit().putString("latitudes" , ObjectSerializer.serialize(latitudes)).apply();
+            sharedPreferences.edit().putString("longitudes" , ObjectSerializer.serialize(longitudes)).apply();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         Toast.makeText(this , "LOCATION SAVED" , Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/example/msjapplication/placemarker/StartActivity.java
+++ app/src/main/java/com/example/msjapplication/placemarker/StartActivity.java
@@ -1,7 +1,9 @@
 package com.example.msjapplication.placemarker;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,6 +17,7 @@ import android.widget.ListView;
 
 import com.google.android.gms.maps.model.LatLng;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import static android.R.id.edit;
@@ -30,11 +33,36 @@ public class StartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start);
 
         ListView listView = (ListView)findViewById(R.id.listView);
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.msjapplication.placemarker" , Context.MODE_PRIVATE);
+        ArrayList<String> latitudes = new ArrayList<>();
+        ArrayList<String> longitudes = new ArrayList<>();
+        places.clear();
+        latitudes.clear();
+        longitudes.clear();
+        locations.clear();
 
-        places.add("Add a new place ...");
-        locations.add(new LatLng(0,0));
+        try {
+
+            places = (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("places" , ObjectSerializer.serialize(new ArrayList<String>())));
+            latitudes = (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("latitudes" , ObjectSerializer.serialize(new ArrayList<String>())));
+            longitudes = (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("longitudes" , ObjectSerializer.serialize(new ArrayList<String>())));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (places.size() > 0 && latitudes.size() > 0 && longitudes.size() > 0){
+            if (places.size() == latitudes.size() && latitudes.size() == longitudes.size()){
+                for (int i = 0 ; i < latitudes.size() ; i++){
+                    locations.add(new LatLng(Double.parseDouble(latitudes.get(i)) , Double.parseDouble(longitudes.get(i))));
+                }
+            }
+        }else {
+
+            places.add("Add a new place ...");
+            locations.add(new LatLng(0, 0));
+        }
         arrayAdapter = new ArrayAdapter(this , android.R.layout.simple_list_item_1 , places);
         listView.setAdapter(arrayAdapter);
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
@@ -43,6 +71,9 @@ public class StartActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+
 
     }
 
